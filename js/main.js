599b3d1ad3b5899b24aef4b164f480cb9d370437
MarcosjoBR <marcosjacquesoliveira.1999@gmail.com>
2018-11-02 20:09:48 -0300

    testando erros

--- js/main.js
+++ js/main.js
@@ -196,14 +196,13 @@ $(document).ready(function() {
   -------------------------------------------------------------------------------*/
 
     function initParallax() {
-    $('#home').parallax("100%", 0.1);
+    $('#header').parallax("100%", 0.1);
     $('#about').parallax("100%", 0.3);
-    $('#service').parallax("100%", 0.2);
-    $('#experience').parallax("100%", 0.3);
-    $('#education').parallax("100%", 0.1);
-    $('#quotes').parallax("100%", 0.3);
-    $('#contact').parallax("100%", 0.1);
-    $('footer').parallax("100%", 0.2);
+    $('#portfolio').parallax("100%", 0.2);
+    $('#journal').parallax("100%", 0.3);
+    $('#venue').parallax("100%", 0.1);
+    $('#footer').parallax("100%", 0.3);
+    
 
   }
   initParallax();
